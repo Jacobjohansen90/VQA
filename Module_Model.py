199515d5d2fdceb66b88293e7415b807553f958b
Jacob <jacobjohansen90@gmail.com>
2019-12-15 04:17:37 +0100

    test

--- Module_Model.py
+++ Module_Model.py
@@ -9,8 +9,44 @@ Created on Wed Oct  2 12:27:55 2019
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from Layers import ResidualBlock, Flatten 
+#from Layers import ResidualBlock, Flatten 
 import Program_funcs as func
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_dim, out_dim=None, with_residual=True,
+                 with_batchnorm=True):
+        if out_dim is None:
+            out_dim = in_dim
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_dim, out_dim, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(out_dim, out_dim, kernel_size=3, padding=1)
+        self.with_batchnorm = with_batchnorm
+        if self.with_batchnorm:
+            self.bn1 = nn.BatchNorm2d(out_dim)
+            self.bn2 = nn.BatchNorm2d(out_dim)
+        self.with_residual = with_residual
+        if in_dim == out_dim or not with_residual:
+            self.proj = None
+        else:
+            self.proj = nn.Conv2d(in_dim, out_dim, kernel_size=1)
+            
+    def forward(self, x):
+        if self.with_batchnorm:
+            out = F.relu(self.bn1(self.conv1(x)))
+        else:
+            out = self.conv2(F.relu(self.conv1(x)))
+        res = x if self.proj is None else self.proj(x)
+        if self.with_residual:
+            out = F.relu(res + out)
+        else:
+            out = F.relu(out)
+        return out
+        
+   
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
 
 class ConcatBlock(nn.Module):
     def __init__(self, dim, with_residual=True, with_batchnorm=True):
@@ -191,11 +227,7 @@ class ModuleNet(nn.Module):
             while len(module_inputs) < num_inputs:
                 cur_input, j = self.forward_modules_ints_helper(feats, program, i, j)
                 module_inputs.append(cur_input)
-        if torch.cuda.current_device() != 0:
-            print(torch.cuda.current_device(), 'before module')
         module_output = module(*module_inputs)
-        if torch.cuda.current_device() != 0:
-            print('after module')
         return module_output, j
     
     def forward_modules_ints(self, feats, program):
