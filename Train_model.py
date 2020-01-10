07c3ab3c60ab0d0b3e1ae8703956c96ca82f9625
Jacob <jacobjohansen90@gmail.com>
2020-01-10 11:38:10 +0100

    MAPO bug fix

--- Train_model.py
+++ Train_model.py
@@ -197,7 +197,7 @@ if __name__ == '__main__':
             'vocab':vocab,
             'batch_size':args.batch_size,
             'shuffle': args.shuffle_train_data,
-            'pin_memory': True,
+            'pin_memory': False,
             'max_samples': num_train_samples,
             'num_workers': args.loader_num_workers,
             'balanced_n':balanced_n,
