9bd2abeed74521476fc3d1adde61e846435f0874
Jacob <jacobjohansen90@gmail.com>
2020-01-10 17:03:30 +0100

    MAPO bug fix

--- Module_Model.py
+++ Module_Model.py
@@ -245,6 +245,8 @@ class ModuleNet(nn.Module):
         elif type(program) is torch.Tensor and program.dim() == 2:
             final_module_outputs = self.forward_modules_ints(feats, program)
         else:
+            print(program)
+            print(program.shape)
             raise ValueError('Unregonized program format')
             
         out = self.classifier(final_module_outputs)
